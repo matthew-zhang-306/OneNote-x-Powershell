c6019605a11c75eaa6afa4c5aa4c0369ce547e3d
matthew-zhang-306 <matthew.zhang.306@gmail.com>
2019-07-15 15:23:58 -0500

    Try/Catch around Execution Policy
    Also sleeps because we need to be able to read the messages

--- Application.ps1
+++ Application.ps1
@@ -5,14 +5,23 @@
         cd $psISE.CurrentFile.FullPath.Replace("\Application.ps1","")
     }
 
-    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+    # Execution policy might not work because of the "more specific scope" error so this try/catch block works around it
+    try {
+        Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+    }
+    catch {
+        Write-Host "Could not set execution policy: "
+        $_
+        Start-Sleep -s 3
+    }
 
     Add-Type -AssemblyName Microsoft.Office.Interop.OneNote
     Add-Type -Path "WinSCP/WinSCPnet.dll"
+
+    & ".\OneNote.ps1"
 }
 catch {
-    Write-Host "Error running: $($_.Exception.Message)"
-}
-finally {
-    & ".\OneNote.ps1"
+    Write-Host "Error running: "
+    $_
+    Start-Sleep -s 15
 }
