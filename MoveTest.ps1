c1b61f746cf8f5e4820fecd9e6fe85a7de790c56
matthew-zhang-306 <matthew.zhang.306@gmail.com>
2019-08-06 17:25:06 -0500

    Create MoveTest.ps1
    Trying out automated page movement, works with empty pages at least

---
+++ MoveTest.ps1
@@ -0,0 +1,63 @@
+﻿using namespace Microsoft.Office.Interop
+using namespace System.Xml
+
+cls
+
+try {
+    $onenote = New-Object -ComObject OneNote.Application
+    $schema = @{one=”http://schemas.microsoft.com/office/onenote/2013/onenote”}
+    [xml]$hierarchy = ""
+    $onenote.GetHierarchy("", [OneNote.HierarchyScope]::hsPages, [ref]$hierarchy)
+
+
+    [XmlElement]$notebook = $null
+    foreach ($notebookXml in $hierarchy.Notebooks.Notebook) {
+        if ($notebookXml.Name.Contains("Sai")) {
+            $notebook = $notebookXml
+        }
+    }
+
+    $fromgroup = $null
+    $togroup = $null
+    foreach ($sectiongroup in $notebook.SectionGroup) {
+        if ($sectiongroup.Name.Contains("Thursday")) {
+            $fromgroup = $sectiongroup
+        }
+        elseif ($sectiongroup.Name.Contains("Friday")) {
+            $togroup = $sectiongroup
+        }
+    }
+
+
+    $tosection = $null
+    foreach ($section in $togroup.Section) {
+        if ($section.Name.Contains("Math")) {
+            $tosection = $section
+        }
+    }
+
+
+    $targetpage = $null
+    $targetcontent = $null
+    foreach ($page in $fromgroup.Section.Page) {
+        if ($page.Name.Contains("to be moved")) {
+            $targetpage = $page
+            $onenote.GetPageContent($page.ID, [ref]$targetcontent)
+        }
+    }
+
+
+    $newpageid = $null
+    $onenote.CreateNewPage($tosection.ID, [ref]$newpageid)
+    $targetcontent = $targetcontent.Replace($targetpage.ID, $newpageid)
+
+
+    Write-Host $targetcontent
+
+
+    $onenote.UpdatePageContent($targetcontent)
+    $onenote.DeleteHierarchy($targetpage.ID)
+}
+catch {
+    $_
+}
